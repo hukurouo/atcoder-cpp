581f344fbfa7f0e9cd944b1c016382e824e89dab
hukurouo <owlbook248@gmail.com>
2022-02-19 01:56:12 +0900

    setup

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  int a;
+ 
+  // 変数aで入力を受け取る
+  cin >> a;
+    for (int i = 0; i < 3; i++)
+    {
+        cout << i << endl;
+    }
+    
+  cout << a * 10 << endl;
+}
